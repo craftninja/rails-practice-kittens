a1f89dd1faee8e4bf4d8a3b6c495abb959f29ce6
EmilyPlatzer <emilyplatzer@gmail.com>
2014-07-27 17:14:12 -0600

    User can add categorizations to kittens

---
+++ app/controllers/categorizations_controller.rb
@@ -0,0 +1,15 @@
+class CategorizationsController < ApplicationController
+
+  def new
+    @kitten = Kitten.find(params[:kitten_id])
+    @categorization = Categorization.new
+  end
+
+  def create
+    @categorization = Categorization.new(kitten_id: params[:kitten_id], category_id: params[:categorization][:category_id])
+    if @categorization.save
+      redirect_to :root
+    end
+  end
+
+end
